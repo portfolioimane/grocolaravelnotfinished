a8da838aad9636c94dd9941c771c156d401b601f
ima <selfloveima.g@gmail.com>
2022-06-04 15:07:21 +0100

    try

--- app/Providers/CredentialServiceProvider.php
+++ app/Providers/CredentialServiceProvider.php
@@ -35,7 +35,7 @@ class CredentialServiceProvider extends ServiceProvider
                 $provider_array[$value->provider] = [
                     'client_id'     => $value->app_id,
                     'client_secret' => $value->app_secret,
-                    'redirect'      => env('APP_URL') . '/' . 'login/' . $value->provider . '/callback',
+                    'redirect'      => 'https://warm-ridge-54087.herokuapp.com/login/google/callback',
                 ];
 
             }
